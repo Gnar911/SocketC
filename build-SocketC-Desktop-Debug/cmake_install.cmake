04962bf517714fa66fbb2c2a42c98aeae8013b06
Gnar911 <dlong5630@gmail.com>
2025-11-06 17:24:26 +0900

    build client

--- build-SocketC-Desktop-Debug/cmake_install.cmake
+++ build-SocketC-Desktop-Debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/gnar911/Desktop/20251106-SocketC
+# Install script for directory: /home/gnar911/Desktop/20251106-SocketC/SocketC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
